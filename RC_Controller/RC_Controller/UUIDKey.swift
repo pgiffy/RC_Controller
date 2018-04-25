b308ac90efa410e85e1a639c127997adb49ef865
Peter Gifford <31419317+pgiffy@users.noreply.github.com>
2018-04-25 11:41:06 -0600

    Add files via upload

---
+++ RC_Controller/RC_Controller/UUIDKey.swift
@@ -0,0 +1,18 @@
+//
+//  UUIDKey.swift
+//  For RC Car Controller
+//  Created by Peter Gifford on 5/10/18.
+//
+//// Based on tutorials be Trevor Beaton
+import CoreBluetooth
+//Uart Service uuid
+
+
+let kBLEService_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+let kBLE_Characteristic_uuid_Tx = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+let kBLE_Characteristic_uuid_Rx = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+let MaxCharacters = 20
+
+let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
+let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
---
+++ RC_Controller/README.md
@@ -0,0 +1,3 @@
+# RC Controller
+BLE iPhone Controller for RC_Car for EGEN310 Montana state.
+Do not use without permission.
